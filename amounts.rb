4a1ce93f495d8963bf97bd11c9df3ec3e3b64fd7
P-manBrown <tachikawa.yuya@outlook.jp>
2021-11-11 14:18:40 +0000

    [Finish] ruby-7

---
+++ amounts.rb
@@ -0,0 +1,4 @@
+amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
+amounts.each do |furuit, amount|
+  puts "#{furuit}は#{amount}個です"
+end
---
+++ lesson7-1.rb
@@ -0,0 +1,7 @@
+puts "計算を始めます"
+puts "２つの数字を入力してください"
+a = gets.to_i
+b = gets.to_i
+puts "計算結果を出力します"
+puts "a*b=#{a * b}"
+puts "計算を終了します"
